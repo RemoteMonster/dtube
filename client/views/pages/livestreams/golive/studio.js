6507e5b6b481b6ba31fa3befa36dfc61d05d5883
JaehoLee <spectrick@gmail.com>
2018-09-06 20:25:43 +0900

    set broadcast ID to streamId session

--- client/views/pages/livestreams/golive/studio.js
+++ client/views/pages/livestreams/golive/studio.js
@@ -1,22 +1,32 @@
-Template.studio.events({
-  'click #startBroadcast': function(event) {
-    const studio = new Remon({
-      config: {
-        credential: Meteor.settings.public.remon.credential,
-        view: {
-          remote: "#studio"
+Template.studio.rendered = function() {
+  Template.instance().studio = new Remon({
+    config: {
+      credential: Meteor.settings.public.remon.credential,
+      view: {
+        remote: "#studio"
+      },
+      media: {
+        audio: true,
+        video: {
+          width: { max: "640", min: "320" },
+          height: { max: "480", min: "240" },
+          frameRate: 30
         },
-        media: {
-          audio: true,
-          video: {
-            width: { max: "640", min: "320" },
-            height: { max: "480", min: "240" },
-            frameRate: 30
-          },
-        }
       }
-    });
-    studio.createCast();
+    },
+    listener: {
+      onCreate(chid) {
+        console.log('streamId set', chid);
+        Session.set("streamId", chid);
+      }
+    }
+  });
+};
+
+Template.studio.events({
+  'click #startBroadcast': function(event, template) {
+    const cast = template.studio.createCast();
+    console.log(cast);
     event.preventDefault();
   },
 });
